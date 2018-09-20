8e0e598e1d3fbd00380ace61164d9abc38e8f107
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-09-20 09:31:22 +0100

    path folders changed

--- java ad exercises/Functions/2.3 Funcionesex2/ex2/main.js
+++
@@ -1,8 +0,0 @@
-//ex2
-"use strict";
-function multiply(a, b, c, d) {
-  var result = (a + b + c + d) / 2;
-  return result;
-}
-var divisionResult = multiply(5, 5, 5, 1); //sumResult vale 7
-alert(divisionResult);
--- java ad exercises/estructuracontrol/ex4/main.js
+++
@@ -1,4 +0,0 @@
-//ex 4 adalab
-for (var i = 1; i < 11; i++) {
-  console.log("Voy por la vuelta " + i);
-}
